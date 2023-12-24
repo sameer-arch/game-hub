716703ba93a005d9a718b9135fc57e540c3ddbd2
sameer-arch <sameeralhaddad38@gmail.com>
2023-12-24 16:52:11 +0100

    Filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -12,10 +12,24 @@
 // 		</>
 // 	);
 // };
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../assets/hooks/useGenres";
+import {
+	Button,
+	HStack,
+	Image,
+	Link,
+	List,
+	ListItem,
+	Spinner,
+	Text,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../assets/hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
-const GenreList = () => {
+
+interface Props {
+	onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
 	const { data, isLoading, error } = useGenres();
 	if (error) return null;
 	if (isLoading) return <Spinner />;
@@ -29,7 +43,13 @@ const GenreList = () => {
 							borderRadius={8}
 							src={getCroppedImageUrl(genre.image_background)}
 						></Image>{" "}
-						<Text fontSize="large">{genre.name}</Text>
+						<Button
+							fontSize="md"
+							variant={"link"}
+							onClick={() => onSelectGenre(genre)}
+						>
+							{genre.name}
+						</Button>
 					</HStack>
 				</ListItem>
 			))}
